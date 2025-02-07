31bd8a5d5abed9611e6784bf081751ffcf2a3931
Georges Elkassouf <georges.elkassouf@gmail.com>
2025-02-07 19:03:38 +0200

    Add files via upload

---
+++ data_validation.py
@@ -0,0 +1,43 @@
+from pydantic import BaseModel, Field, model_validator
+from datetime import datetime
+
+class StockData(BaseModel):
+    ticker: str = Field(alias="T", 
+                        description="Stock ticker symbol (e.g., AAPL, TSLA)", 
+                        default="")
+    volume_weighted_avg_price: float = Field(alias="vw", 
+                                             description="Volume-weighted average price", 
+                                             default=0.0)
+    open_price: float = Field(alias="o", 
+                              description="Opening price of the stock", 
+                              default=0.0)
+    close_price: float = Field(alias="c", 
+                               description="Closing price of the stock", 
+                               default=0.0)
+    volume: float = Field(alias="v",
+                        description="Total trading volume", 
+                        default=0.0)
+    high_price: float = Field(alias="h", 
+                              description="Highest price during the trading session", 
+                              default=0.0)
+    low_price: float = Field(alias="l", 
+                             description="Lowest price during the trading session", 
+                             default=0.0)
+    date: str = Field(alias="t", 
+                      description="Date of the stock data as timestamp", 
+                      default="")
+    number_of_trades: int = Field(alias="n", 
+                                  description="Total number of trades executed", 
+                                  default=0)
+
+    data_ingestion_timestamp: str = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+
+    @model_validator(mode='before')
+    def validate_data(cls, values):
+        if "t" in values:
+            try:
+                timestamp = int(values["t"] / 1000)  # Convert milliseconds to seconds
+                values["t"] = datetime.fromtimestamp(timestamp).strftime('%Y-%m-%d')  # Format as YYYY-MM-DD
+            except Exception as e:
+                raise ValueError(f"Invalid timestamp: {values['t']}. Error: {e}")
+        return values
