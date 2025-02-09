c2e0a75f3147a2b914ee300ca80ed2d778e71eb2
Georges Elkassouf <georges.elkassouf@gmail.com>
2025-02-09 18:34:54 +0200

    Update combine_csvs.py

--- combine_csvs.py
+++ combine_csvs.py
@@ -39,7 +39,7 @@ client = bigquery.Client(credentials=credentials, project=credentials.project_id
 
 # Define BigQuery dataset and table
 dataset_id = 'bionic-aspect-450214-c2.stock_data'
-table_id = f'{dataset_id}.data'
+table_id = f'{dataset_id}.historical_data'
 
 
 table = client.get_table(table_id)
