621b0e7189d57d06e4801439333364d19d0242f8
Georges Elkassouf <georges.elkassouf@gmail.com>
2025-02-09 18:50:21 +0200

    Create tickers_details_validation.py

---
+++ data_validation/tickers_details_validation.py
@@ -0,0 +1,39 @@
+from datetime import datetime
+from pydantic import BaseModel, Field, model_validator
+
+class TickerDetails(BaseModel):
+    ticker: str = Field(alias="ticker",
+                        default="")
+
+    name: str = Field(alias="name",
+                      default="")
+
+    locale: str = Field(alias="locale",
+                        default="")
+
+    primary_exchange: str = Field(alias="primary_exchange",
+                                  default="")
+
+    currency_name: str = Field(alias="currency_name",
+                               default="")
+
+    market_cap: float = Field(alias="market_cap",
+                              default=0.0)
+
+    description: str = Field(alias="description",
+                             default="")
+
+    homepage_url: str = Field(alias="homepage_url",
+                              default="")
+
+    total_employees: int = Field(alias="total_employees",
+                                 default=0)
+
+    list_date: str = Field(alias="list_date",
+                           default="")
+
+    @model_validator(mode='before')
+    def validate_data(cls, values):
+        if "list_date" in values:
+            values["list_date"] = datetime.strptime(values["list_date"], "%Y-%m-%d").strftime('%Y-%m-%d %H:%M:%S')
+        return values
