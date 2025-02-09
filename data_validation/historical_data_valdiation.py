7d88007ce36fde6fb40bd8501d0ed030bb5bc331
Georges Elkassouf <georges.elkassouf@gmail.com>
2025-02-09 13:43:43 +0200

    Create historical_data_valdiation.py

---
+++ data_validation/historical_data_valdiation.py
@@ -0,0 +1,37 @@
+from pydantic import BaseModel, Field, model_validator
+
+class HistoricalData(BaseModel):
+    ticker: str = Field(alias="ticker",
+                        default="")
+
+    date: str = Field(alias="date",
+                      default="")
+
+    open_price: float = Field(alias="open_price",
+                              default=0.0)
+
+    close_price: float = Field(alias="close_price",
+                               default=0.0)
+
+    volume: int = Field(alias="volume",
+                          default=0)
+
+    high_price: float = Field(alias="high_price",
+                              default=0.0)
+
+    low_price: float = Field(alias="low_price",
+                             default=0.0)
+
+
+    @model_validator(mode='before')
+    def validate_data(cls, values):
+        for field in ['open_price', 'close_price', 'high_price', 'low_price']:
+            if field in values:
+                value = values[field]
+                if isinstance(value, str):  # Check if the value is a string
+                    value = value.replace('$', '').replace(',', '').strip()  # Remove '$', ',' and whitespace
+                    try:
+                        values[field] = float(value)  # Convert to float
+                    except ValueError:
+                        raise ValueError(f"Could not convert {value} to float for {field}")
+        return values
