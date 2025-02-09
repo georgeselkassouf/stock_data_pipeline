a3061865524bcfb9f60a64ba0dd64ea37ff58065
Georges Elkassouf <georges.elkassouf@gmail.com>
2025-02-09 13:17:11 +0200

    Add files via upload

---
+++ tickers_list.py
@@ -0,0 +1,4 @@
+tickers = [
+    'AAPL', 'MSFT', 'NVDA', 'GOOGL', 'AMZN', 'META', 'TSLA', 'ASML', 'ORCL', 'COST', 'CSCO',
+    'MCD', 'DHR', 'NKE', 'ADBE', 'TMO', 'DIS', 'NFLX', 'INTC', 'SBUX', 'AMD', 'HON', 'AMGN','LOW', 'MDT'
+]
