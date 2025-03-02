4b8c72849c4203c11ba8fd0f19a56118002bc384
Georges Kassouf <georges.elkassouf@gmail.com>
2025-03-02 10:44:11 +0200

    updates tickers list and historical data csvs

--- tickers_list.py
+++ tickers_list.py
@@ -2,5 +2,5 @@ tickers = [
     "AAPL", "NVDA", "MSFT", "AMZN", "GOOGL", "META",
     "TSLA", "AVGO", "TSM", "JPM",
     "WMT", "LLY", "V", "MA", "UNH", "ORCL",
-    "XOM", "COST", "BAC", "HD", "PG", "NFLX", "JNJ", "NVO"
+    "XOM", "COST", "BAC", "HD", "PG", "NFLX", "JNJ", "NVO", "BRK.A"
 ]
